23291a6da5c65b505167547bd024c8445dde2dd8
franco paez <francopaez_11@hotmail.com>
2025-07-03 23:19:59 -0300

    primer commit

---
+++ src/components/PrimerComponente/PrimerComponente.jsx
@@ -0,0 +1,10 @@
+import SegundoComponente from "../SegundoComponente/SegundoComponente"
+const PrimerComponente=()=>{
+    return(
+        <>
+        <h1>hola  <SegundoComponente  nombre="franco" edad={28} /></h1>
+        </>
+    )
+}
+
+export default PrimerComponente
---
+++ src/components/SegundoComponente/SegundoComponente.jsx
@@ -0,0 +1,14 @@
+const SegundoComponente=({nombre,edad})=>{
+
+
+  
+   
+    return(
+        <>
+        <p>mi nombre es {nombre}</p>
+        <p>y mi edad es {edad} años</p>
+        </>
+    )
+}
+
+export default SegundoComponente
